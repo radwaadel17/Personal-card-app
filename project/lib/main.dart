2746fa7c24b87e683c24a685377409226aa94115
radwa <radwaadel959@gmail.com>
2024-05-07 00:33:17 +0300

    Add photo as a circle

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main (){
+void main() {
   runApp(PersonalCard());
 }
 
 class PersonalCard extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-         home: Scaffold(
-          backgroundColor: Colors.black,
-         ) ,
+      home: Scaffold(
+        backgroundColor: Color(0xFF151515),
+        body: Column(
+          children: [
+            CircleAvatar(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('Images/logo.jpg'),
+                radius: 115,
+              ),
+              radius: 118,
+              backgroundColor: Colors.white,
+            ),
+
+            
+          ],
+        ),
+      ),
     );
   }
 }
